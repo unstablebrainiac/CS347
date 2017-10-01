32a53fd1f8a19f2ab06d2024f059af6d65294c35
Darshan <darshantank3@gmail.com>
2017-10-01 23:12:35 +0530

    ass1 done

--- Ass2_Part1_Q1.cpp
+++ Ass2_Part1_Q1.cpp
@@ -1,23 +1,21 @@
-// C program to demonstrate use of fork() and pipe()
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
 #include<sys/types.h>
 #include<string.h>
 #include<sys/wait.h>
+#include<vector>
  
 int main()
 {
-    // We use two pipes
-    // First pipe to send input string from parent
-    // Second pipe to send concatenated string from child
- 
-    int fd1[2];  // Used to store two ends of first pipe
-    int fd2[2];  // Used to store two ends of second pipe
- 
-    char fixed_str[] = "forgeeks.org";
-    char input_str[100];
-    pid_t p;
+    int fd1[2];
+    int fd2[2];
+
+    int fd3[2];
+    int fd4[2];
+
+    int n;
+    pid_t p; int b, c;
  
     if (pipe(fd1)==-1)
     {
@@ -29,65 +27,176 @@ int main()
         fprintf(stderr, "Pipe Failed" );
         return 1;
     }
- 
-    scanf("%s", input_str);
-    p = fork();
- 
-    if (p < 0)
+    if (pipe(fd3)==-1)
     {
-        fprintf(stderr, "fork Failed" );
+        fprintf(stderr, "Pipe Failed" );
+        return 1;
+    }
+    if (pipe(fd4)==-1)
+    {
+        fprintf(stderr, "Pipe Failed" );
         return 1;
     }
  
-    // Parent process
-    else if (p > 0)
-    {
-        char concat_str[100];
- 
-        close(fd1[0]);  // Close reading end of first pipe
- 
-        // Write input string and close writing end of first
-        // pipe.
-        write(fd1[1], input_str, strlen(input_str)+1);
-        close(fd1[1]);
- 
-        // Wait for child to send a string
-        wait(NULL);
- 
-        close(fd2[1]); // Close writing end of second pipe
- 
-        // Read string from child, print it and close
-        // reading end.
-        read(fd2[0], concat_str, 100);
-        printf("Concatenated string %s\n", concat_str);
-        close(fd2[0]);
+    scanf("%d", &n);
+
+    p = fork();
+    b = (int) p;
+
+    if(b >0){
+        p = fork();
+        c = (int) p;
     }
- 
-    // child process
-    else
+
+    if (b > 0 && c>0)
     {
-        close(fd1[1]);  // Close writing end of first pipe
- 
-        // Read a string using first pipe
-        char concat_str[100];
-        read(fd1[0], concat_str, 100);
- 
-        // Concatenate a fixed string with it
-        int k = strlen(concat_str);
-        int i;
-        for (i=0; i<strlen(fixed_str); i++)
-            concat_str[k++] = fixed_str[i];
- 
-        concat_str[k] = '\0';   // string ends with '\0'
- 
-        // Close both reading ends
-        close(fd1[0]);
+        //parent
+        if(c>0){
+
+            close(fd1[0]);
+            close(fd3[0]);
+
+            int t=0;
+            for(int i=2; i<n; i++){
+
+                if(i%2!=0 && i%3!=0 && i%5!=0 && i%7!=0 && i%11!=0){
+                    if(t==0){
+                        write(fd1[1], &i, sizeof(i));
+                        t=1;
+                    }
+                    else{
+                        write(fd3[1], &i, sizeof(i));
+                        t=0;
+                    }
+                }
+
+            }
+
+            close(fd1[1]);
+            close(fd3[1]);
+
+            wait(NULL);
+
+            int initial_primes[5]={2,3,5,7,11};
+            int index=0;
+            while(initial_primes[index]<=n && index<=4){
+                printf("%d\n", initial_primes[index]);
+                index++;
+            }
+     
+            close(fd2[1]);
+            close(fd4[1]);
+
+            int temp1, temp2;
+            std::vector<int> vec1, vec2;
+
+            while(read(fd2[0], &temp1,sizeof(temp1))>0){
+                vec1.push_back(temp1);
+            }
+            while(read(fd4[0], &temp2,sizeof(temp2))>0){
+                vec2.push_back(temp2);
+            }
+
+            //merge
+
+            int i = 0, j=0, k=0;
+            int n1 =vec1.size();
+            int n2 =vec2.size();
+
+            while (i < n1 && j < n2)
+            {
+                if (vec1[i] < vec2[j])
+                {
+                    printf("%d\n", vec1[i] );
+                    i++;
+                }
+                else
+                {
+                    printf("%d\n", vec2[j] );
+                    j++;
+                }
+            }
+
+            while (i < n1)
+            {
+                printf("%d\n", vec1[i] );
+                i++;
+            }
+
+            while (j < n2)
+            {
+                printf("%d\n", vec2[j] );
+                j++;
+            }
+
+
+            close(fd2[0]);
+            close(fd4[0]);
+
+            return 0; 
+        }
+        
+    }
+    
+    // child process
+    else if( b == 0 )
+    {
+        int temp1;
+
+
+        close(fd3[0]);        
+        close(fd3[1]);
+        close(fd4[0]);        
+        close(fd4[1]);
+
+        close(fd1[1]);
         close(fd2[0]);
- 
-        // Write concatenated string and close writing end
-        write(fd2[1], concat_str, strlen(concat_str)+1);
+
+        while(read(fd1[0], &temp1,sizeof(temp1))>0){
+            int flag = 0;
+            for(int i=13; i<(temp1/2); i++){
+                if(temp1%i==0){
+                    flag =1;
+                    break;
+                }
+            }
+            if (!flag)
+            {
+                write(fd2[1], &temp1, sizeof(temp1));
+            }
+        }
+        close(fd1[0]);
         close(fd2[1]);
- 
         exit(0);
     }
+    //second child
+    else if(b>0 && c==0){
+        int temp2;
+
+        close(fd1[0]);        
+        close(fd1[1]);
+        close(fd2[0]);        
+        close(fd2[1]);
+
+        close(fd3[1]);
+        close(fd4[0]);
+
+        while(read(fd3[0], &temp2,sizeof(temp2))>0){
+            int flag = 0;
+            for(int i=13; i<(temp2/2); i++){
+                if(temp2%i==0){
+                    flag =1;
+                    break;
+                }
+            }
+            if (!flag)
+            {
+                write(fd4[1], &temp2, sizeof(temp2));
+            }
+        }
+        close(fd3[0]);
+        close(fd4[1]);
+        exit(0);
+    }
+
 }
